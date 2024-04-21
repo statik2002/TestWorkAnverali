60f38644d077656cb71105aa469c1a8d6047f999
statik2002 <statik2002@mail.ru>
2024-04-21 14:30:15 +0800

    release 1.1

--- kwork/cabinet/views.py
+++ kwork/cabinet/views.py
@@ -1,11 +1,22 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from main.models import Customer
+
 
 @login_required
 def index(request):
-    context = {
+    if request.user.is_customer:
+        freelancers = Customer.objects.filter(is_customer=False)
 
-    }
+        context = {
+            'freelancers': freelancers
+        }
+
+    else:
+        customers = Customer.objects.filter(is_customer=True)
+        context = {
+            'customers': customers
+        }
 
     return render(request, 'cabinet/index.html', context)
